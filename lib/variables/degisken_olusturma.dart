a8fc94977f819626088e742d96c24e301826c322
burakcetn <burakcetn@gmail.com>
2023-10-26 13:10:39 +0300

    first commit

---
+++ lib/variables/degisken_olusturma.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {}
+
+class StudentConstants {
+  String name = "Burak";
+  int age = 26;
+  double length = 1.78;
+  String firstLetter = "B";
+  bool isContinue = true;
+}
+
+class ProductInfo {
+  int urun_id = 3416;
+  String urun_adi = "Kol Saati";
+  int urun_adet = 100;
+  double urun_fiyat = 149.99;
+  String urun_tedarikci = "Rolex";
+}
